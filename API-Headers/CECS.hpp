40f107848e825df00eeaa10caad2a3c2379abf42
terablade2001 <terablade2001@yahoo.gr>
2024-12-23 21:23:55 +0200

    (0.01)[I#1]-{Work on basic design. Setup utilities etc.}: Making...
    > https://github.com/terablade2001/CECS2/issues/1

--- API-Headers/CECS.hpp
+++ API-Headers/CECS.hpp
@@ -1,2 +1,17 @@
 #pragma once
+#include <string>
 
+class CECS {
+public:
+  CECS() = delete;
+  explicit CECS(const std::string& modNameStr_ = "", const std::string& ecsNameStr_ = "");
+  CECS(const CECS& obj)  = delete; // No copy constructor.
+  ~CECS();
+
+  static CECS* getInstance();
+
+private:
+  static CECS* instancePtr;
+  std::string ecsName;
+  std::string modName;
+};
