7989ecd165cf35db0f3e99a5dbdb6167011ddec9
terablade2001 <terablade2001@yahoo.gr>
2024-12-23 22:36:09 +0200

    (0.03)[I#1]: Now tests passes ok.

--- unitTests/testCECSSingleton.cpp
+++ unitTests/testCECSSingleton.cpp
@@ -4,7 +4,7 @@
 
 
 namespace docTests {
-  DOCTEST_TEST_SUITE("Basic Math Operations") {
+  DOCTEST_TEST_SUITE("Basic Operations") {
     TEST_CASE("Test CECS Singleton") {
       const BaseCECS * ecsPtr = BaseCECS::getInstance("testECS");
       CHECK_NE(ecsPtr, nullptr);
